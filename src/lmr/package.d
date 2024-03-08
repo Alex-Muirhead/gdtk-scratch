c658560343f4f4ef6aa33078486eb9434ded1771
Alex Muirhead <a.muirhead@uq.net.au>
2024-03-08 17:09:52 +1000

    Add to lmr dub to allow for importing

---
+++ src/lmr/package.d
@@ -0,0 +1,10 @@
+module lmr;
+
+public import flowsolution;
+public import init;
+public import lmr.fvcell;
+
+public import globalconfig;
+public import globaldata;
+public import lmrconfig;
+public import fileutil;
