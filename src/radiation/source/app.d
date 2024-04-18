dfa991b4834839ba33eb56e019a1870838d5ed9e
Alex Muirhead <a.muirhead@uq.net.au>
2024-04-18 15:22:10 +1000

    Extract full ray marching to function

--- src/radiation/source/app.d
+++ src/radiation/source/app.d
@@ -1,26 +1,93 @@
-import std.math : cos, sin, PI;
+import std.math : cos, sin, PI, pow;
 import std.stdio;
 import std.random : Random, uniform;
 
 import lmr.config;
 import lmr.fileutil : ensure_directory_is_present;
-import lmr.fvcell;
+import lmr.fluidblock : FluidBlock;
+import lmr.fluidfvcell : FluidFVCell;
+import lmr.fvcell : FVCell;
 import lmr.globalconfig;
 import lmr.globaldata;
 import lmr.init;
-import lmr.ufluidblock;
 import lmr.sfluidblock;
+import lmr.ufluidblock;
 
-import geom.elements.vector3;
+import geom.elements.vector3 : Vector3;
 import geom.grid.grid;
+import nm.number;
 
 import gas.physical_constants : StefanBoltzmann_constant;
+
+/// March across a block from the given cell in desired direction. 
+/// This **full** implementation takes many small steps across the domain
+/// FIXME: The step size should probably be dependent on cell size...
+void marching_full(
+    size_t cellID, FluidBlock block, Vector3 direction, 
+    ref size_t[] crossed, ref number[] lengths) 
+{
+    bool isWithinBlock = true;
+    bool isContained;
+    FluidFVCell currentCell = block.cells[cellID];
+    Vector3 rayCoord = currentCell.pos[0];  // INFO: Grid is stationary, index is time
+    Grid currentGrid = get_grid(block);
+
+    number stepSize = 1E-03; // FIXME: This should be proportional to something...
+    int consecutiveSteps = 1;
+
+    // NOTE: Do we want to record the first step?
+    
+    marchLoop: while (isWithinBlock) {
+        rayCoord += stepSize * direction;
+        isContained = currentGrid.point_is_inside_cell(rayCoord, currentCell.id);
+
+        if (isContained) {
+            consecutiveSteps++;
+            continue;
+        }
+
+        // Record current steps
+        crossed ~= currentCell.id;
+        lengths ~= stepSize * consecutiveSteps;
+        consecutiveSteps = 1;
+
+        foreach (neighbour; currentCell.cell_cloud) {
+            if (neighbour.is_ghost) {
+                // FIXME: Deal with ghost cells!
+                continue;
+            }
+            isContained = currentGrid.point_is_inside_cell(rayCoord, neighbour.id);
+            if (isContained) {
+                currentCell = neighbour;
+                continue marchLoop;
+            }
+        }
+
+        if (!isContained) {
+            // We haven't found the neighbour
+            break marchLoop;
+        }
+    }
+
+}
+
+Grid get_grid(FluidBlock block) {
+    final switch (block.grid_type) {
+        case Grid_t.structured_grid:
+            SFluidBlock sblk = cast(SFluidBlock) block;
+            return sblk.grid;
+        case Grid_t.unstructured_grid:
+            UFluidBlock ublk = cast(UFluidBlock) block;
+            return ublk.grid;
+    }
+}
 
 void main() {
     writeln("Initial workings on a standalone radiation post-processing code.");
 
     // Hard-code how many snapshots we're working with
-    auto snapshotStart = 0003;
+    uint snapshotStart = 0003;
+    uint nWrittenSnapshots = snapshotStart + 1;
 
     alias cfg = GlobalConfig;
 
@@ -32,103 +99,42 @@ void main() {
     initFluidBlocksZones();
     initFluidBlocksFlowField(snapshotStart);
 
-    int nWrittenSnapshots = 4;
-
-    Grid[] blockGrids = [];
-    foreach (blk; localFluidBlocks) {
-        foreach (cell; blk.cells) {
-            // Set everything to zero to be sure
-            cell.fs.Qrad = 0.0;
-        }
-
-        final switch (blk.grid_type) {
-            case Grid_t.structured_grid:
-                SFluidBlock sblk = cast(SFluidBlock) blk;
-                blockGrids ~= sblk.grid;
-                break;
-            case Grid_t.unstructured_grid:
-                UFluidBlock ublk = cast(UFluidBlock) blk;
-                blockGrids ~= ublk.grid;
-                break;
-        }
-    }
-    writeln("Grid? ", blockGrids);
-
     auto dirName = snapshotDirectory(nWrittenSnapshots);
     ensure_directory_is_present(dirName);
 
     auto rng = Random(4);  // Chosen by fair dice roll guaranteed to be random (xkcd.com/221)
 
-    auto decay = 0.01; // NOTE: Should be proportional to the step size
-    auto stepLength = 0.001;
-
+    auto decay = 0.1;
     auto firstBlock = localFluidBlocks[0];
+    uint angleSamples = 100;
 
     foreach (cell_id, ref origin; firstBlock.cells) {
 
         // auto angle = uniform(0.0f, 2*PI, rng);
+        number fullEmission = StefanBoltzmann_constant * origin.fs.gas.T ^^ 4;
 
-        for (auto a = 0.0; a < 1; a += 0.01) {
+        foreach (a; 0 .. angleSamples) {
             // Loop through angles for now
-            auto angle = 2*PI * a;
+            auto angle = 2*PI * a / angleSamples;
             auto direction = Vector3([cos(angle), sin(angle), 0]);
             direction.normalize(); // Shouldn't be needed with random angle
 
-            writeln("Moving at angle: ", angle);
+            auto rayStrength = fullEmission / angleSamples;
+            origin.fs.Qrad -= rayStrength;  // Remove the energy of the ray emitted
 
-            auto cell = origin;
-            Vector3 rayPoint = cell.pos[0]; // Grid isn't moving, take the first position
+            size_t[] rayCells;
+            number[] rayLengths;
+            marching_full(cell_id, firstBlock, direction, rayCells, rayLengths);
+            writeln("Stepped over ", rayCells.length, " cells");
 
-            auto rayStrength = StefanBoltzmann_constant * cell.fs.gas.T ^^ 4;
-            cell.fs.Qrad -= rayStrength;  // Remove the energy of the ray emitted
-
-            steps: foreach (i; 0 .. 1000) {
-                rayPoint += direction * stepLength;
-                auto heating = decay * rayStrength;
+            number heating;
+            foreach (i; 0 .. rayCells.length) {
+                heating = rayStrength * (1 - (1 - decay) ^^ rayLengths[i]);
                 rayStrength -= heating;
-
-                bool contained = blockGrids[0].point_is_inside_cell(rayPoint, cell_id);
-                if (!contained) {
-                    size_t neighbour_id;
-                    inner: foreach (neighbour; cell.cell_cloud) {
-                        neighbour_id = neighbour.id;
-                        Vector3[][] vertices = [];
-                        foreach (j, ref vertex; neighbour.vtx) {
-                            vertices ~= vertex.pos;
-                        }
-                        if (neighbour.is_ghost) {
-                            // FIXME: Deal with ghost cells at the boundaries
-                            continue inner;
-                        }
-                        if (blockGrids[0].point_is_inside_cell(rayPoint, neighbour_id)) {
-                            contained = true;
-                            break inner;
-                        }
-                    }
-                    if (contained) {
-                        // Successfully found next point
-                        cell_id = neighbour_id;
-                        cell = firstBlock.cells[cell_id];
-                    } else {
-                        // Could not find next cell
-                        // writeln("Error! Couldn't find the thing");
-                        break steps;
-                    }
-                }
-                cell.fs.Qrad += heating * 10;
-
-                // This has to be at the end, since we have to find *where* to dump the 
-                // energy before ending the routine. 
-                if (rayStrength < 1E-3) { // Some minimum energy threshold
-                    break steps;
-                }
+                firstBlock.cells[rayCells[i]].fs.Qrad += heating;
             }
         }
     }
-    // The method `geom.grid.usgrid.UnstructuredGrid.get_list_of_boundary_cells`
-    // looks at what is inside a cell vs outside
-    // There is also the method `geom.grid.grid.Grid.point_is_inside_cell`
-    // How do we get access to the grid though?
     
     foreach (blk; localFluidBlocks) {
         auto fileName = fluidFilename(nWrittenSnapshots, blk.id);
