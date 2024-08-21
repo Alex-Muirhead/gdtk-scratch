7f925c6286e0da7263ddba5b3026e7ef748fb468
Alex Muirhead <a.muirhead@uq.net.au>
2024-08-21 13:54:04 +1000

    Move tangentslab to module

--- src/radiation/source/app.d
+++ src/radiation/source/app.d
@@ -1,7 +1,7 @@
 import std.algorithm.mutation : swap;
-import std.algorithm: min, max;
+import std.algorithm : min, max;
 import std.array;
-import std.math : cos, sin, PI, pow, exp, fabs;
+import std.math : cos, sin, PI, pow, fabs;
 import std.stdio;
 import std.getopt;
 
@@ -25,19 +25,7 @@ import lmr.sfluidblock;
 import lmr.ufluidblock;
 
 import raytracing;
-
-
-/// A two term exponential approximation to the 3rd exponential
-/// integral function E3(x)
-///
-/// Params:
-///   x = Positive real input value
-/// Returns: 
-///   Approximate value
-number exponential_integral(number x) {
-    return 0.0929*exp(-4.08*x) + 0.4071*exp(-1.33*x);
-}
-
+import tangentslab;
 
 void main(string[] args) {
 
@@ -94,15 +82,13 @@ void main(string[] args) {
     foreach (n, iface; capsuleWall.faces) {
 
         // Start tangent trace routine
-        
+
         size_t[] crossed = [];
         number[] lengths = [];
 
         // naive_tangent_marching(iface.left_cell.id, block, direction, crossed, lengths);
         // NOTE: Need to switch interface SIDE and normal SIGN when going from West to East etc.
         marching_efficient(iface.right_cell.id, block, iface.n, crossed, lengths);
-
-        // End tangent trace routine
 
         number[] cellIntensity = [];
         number[] opticalThickness = [];
@@ -112,36 +98,11 @@ void main(string[] args) {
             opticalThickness ~= absorptionCoefficient * lengths[i];
             cellIntensity ~= block.cells[crossed[i]].grey_blackbody_intensity();
         }
-        
-        number heatFlux;
-        number halfThickness;
-        number opticalDistance;
+
+        number[] cellHeating = tangent_slab_heating(cellIntensity, opticalThickness, lengths);
 
         foreach (i, cellID; crossed) {
-            // Do self-heating case here (i == j)
-            halfThickness = opticalThickness[i] / 2;
-            heatFlux = cellIntensity[i] * (1 - 2 * exponential_integral(halfThickness));
-
-            // Walk away from cell i (along negative coordinate)
-            opticalDistance = halfThickness;
-            for (auto j = long(i) - 1; j >= 0; --j) {
-                heatFlux += cellIntensity[j] * (
-                    exponential_integral(opticalDistance) - 
-                    exponential_integral(opticalDistance + opticalThickness[j])
-                );
-                opticalDistance += opticalThickness[j];
-            }
-            // Walk away from cell i (along positive coordinate)
-            opticalDistance = halfThickness;
-            for (auto j = i + 1; j < crossed.length; ++j) {
-                heatFlux += cellIntensity[j] * (
-                    exponential_integral(opticalDistance) -
-                    exponential_integral(opticalDistance + opticalThickness[j]) 
-                );
-                opticalDistance += opticalThickness[j];
-            }
-
-            block.cells[cellID].fs.Qrad = heatFlux;
+            block.cells[cellID].fs.Qrad = cellHeating[i];
         }
     }
 
@@ -168,15 +129,17 @@ FVInterface naive_tangent_marching(
     while (true) {
         crossed ~= currentCell.id;
         lengths ~= distance_between(
-            currentCell.iface[rayDirection].pos, 
+            currentCell.iface[rayDirection].pos,
             currentCell.iface[opposite_face(rayDirection)].pos);
 
         // This appears to be the best way to check if we hit an edge
         // But it will also trigger if we cross a block boundary
-        if (currentCell.iface[rayDirection].is_on_boundary) { break; }
+        if (currentCell.iface[rayDirection].is_on_boundary) {
+            break;
+        }
 
         // Cell cloud is offset by 1, as the cloud contains the cell itself
-        currentCell = currentCell.cell_cloud[rayDirection+1];
+        currentCell = currentCell.cell_cloud[rayDirection + 1];
     }
 
     return currentCell.iface[rayDirection];
