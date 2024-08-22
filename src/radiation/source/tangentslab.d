db2762809480a3af107dbac6ceac2d52e9bb4e51
Alex Muirhead <a.muirhead@uq.net.au>
2024-08-22 10:59:13 +1000

    Add test for tangent slab and correct method

--- src/radiation/source/tangentslab.d
+++ src/radiation/source/tangentslab.d
@@ -1,6 +1,6 @@
 module tangentslab;
 
-import std.math : exp;
+import std.math : exp, PI;
 
 import nm.number;
 
@@ -16,13 +16,11 @@ number exponential_integral(number x) {
 }
 
 number[] tangent_slab_heating(
-    number[] cellIntensity,
-    number[] opticalThickness,
-    number[] lengths
+    number[] intensity,
+    number[] opticalThickness
 ) {
-    size_t numCells = lengths.length;
-    number[] heating;
-    heating.length = numCells;
+    size_t numCells = opticalThickness.length;
+    number[] heating = new number[numCells];
 
     // Local variables
     number heatFlux;
@@ -32,12 +30,12 @@ number[] tangent_slab_heating(
     foreach (i; 0 .. numCells) {
         // Do self-heating case here (i == j)
         halfThickness = opticalThickness[i] / 2;
-        heatFlux = cellIntensity[i] * (1 - 2 * exponential_integral(halfThickness));
+        heatFlux = intensity[i] * (1 - 2 * exponential_integral(halfThickness));
 
         // Walk away from cell i (along negative coordinate)
         opticalDistance = halfThickness;
         for (auto j = long(i) - 1; j >= 0; --j) {
-            heatFlux += cellIntensity[j] * (
+            heatFlux += intensity[j] * (
                 exponential_integral(
                     opticalDistance) -
                     exponential_integral(opticalDistance + opticalThickness[j])
@@ -47,7 +45,7 @@ number[] tangent_slab_heating(
         // Walk away from cell i (along positive coordinate)
         opticalDistance = halfThickness;
         for (auto j = i + 1; j < numCells; ++j) {
-            heatFlux += cellIntensity[j] * (
+            heatFlux += intensity[j] * (
                 exponential_integral(
                     opticalDistance) -
                     exponential_integral(opticalDistance + opticalThickness[j])
@@ -55,8 +53,43 @@ number[] tangent_slab_heating(
             opticalDistance += opticalThickness[j];
         }
 
-        heating[i] = heatFlux;
+        heating[i] = 2 * PI * heatFlux;
     }
 
     return heating;
+}
+
+@("Constant Temperature")
+unittest {
+
+    import std.stdio;
+    import std.range;
+    import std.algorithm.iteration;
+    import std.math.operations;
+
+    const size_t numCells = 10;
+
+    number[numCells] intensity = 1.0;
+    number[numCells] opticalThickness = 1.0 / numCells;
+    number[numCells] cellCenters;
+
+    cellCenters[0] = opticalThickness[0] / 2;
+    for (auto i = 1; i < numCells; ++i) {
+        cellCenters[i] = cellCenters[i-1] + (opticalThickness[i] + opticalThickness[i-1]) / 2;
+    }
+
+    // D is so weird, wtf do I need to slice a static array to sum it
+    number opticalLength = opticalThickness[].sum();
+
+    number[numCells] heating = tangent_slab_heating(intensity, opticalThickness);
+
+    for (auto i = 0; i < numCells; ++i) {
+        // Analytical solution
+        number expected = 2 * PI * (
+            2 * exponential_integral(0) - 
+            exponential_integral(cellCenters[i]) - 
+            exponential_integral(opticalLength - cellCenters[i])
+        );
+        assert(isClose(heating[i], expected, 1e-6, 1e-6));
+    }
 }
