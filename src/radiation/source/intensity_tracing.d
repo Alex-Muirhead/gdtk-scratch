9b1eb419e4f790e47b04dcf4acb1dc129ffac186
Alex Muirhead <a.muirhead@uq.net.au>
2025-03-27 17:09:19 +1000

    WIP add an intensity solver

---
+++ src/radiation/source/intensity_tracing.d
@@ -0,0 +1,104 @@
+import std.algorithm.mutation : swap;
+import std.algorithm : min, max;
+import std.array;
+import std.format;
+import std.math : cos, sin, PI, pow, fabs;
+import std.stdio;
+import std.getopt;
+
+import geom.elements.nomenclature : Face, opposite_face;
+import geom.elements.vector3 : Vector3, distance_between;
+import gas.physical_constants : StefanBoltzmann_constant;
+import nm.number;
+
+import lmr.config;
+import lmr.bc.boundary_condition : BoundaryCondition;
+import lmr.bc.ghost_cell_effect.full_face_copy : GhostCellFullFaceCopy;
+import lmr.fileutil : ensure_directory_is_present;
+import lmr.fluidblock : FluidBlock;
+import lmr.fluidfvcell : FluidFVCell;
+import lmr.fvcell : FVCell;
+import lmr.fvinterface : FVInterface;
+import lmr.globalconfig;
+import lmr.globaldata;
+import lmr.init;
+import lmr.sfluidblock;
+import lmr.ufluidblock;
+
+import raytracing;
+
+void main(string[] args) {
+
+    // Default arg values
+    string workingDir = ".";
+    double absorptionCoefficient = 1.0;
+
+    auto helpInformation = getopt(
+        args, std.getopt.config.stopOnFirstNonOption,
+        "d|dir", &workingDir,
+        "k|absorptivity", &absorptionCoefficient
+    );
+
+    if (helpInformation.helpWanted) {
+        defaultGetoptPrinter("lmr-raytrace options.", helpInformation.options);
+        return;
+    }
+
+    writeln("Initial workings on a standalone radiation post-processing code.");
+    writeln(format("Absorptivity: %.2g", absorptionCoefficient));
+
+    // Hard-code how many snapshots we're working with
+    uint snapshotStart = 0;
+    uint nWrittenSnapshots = snapshotStart + 1;
+
+    alias cfg = GlobalConfig;
+
+    initConfiguration();
+    initLocalBlocks();
+    initFluidBlocksBasic();
+    initFluidBlocksMemoryAllocation();
+    initFluidBlocksGlobalCellIDStarts();
+    initFluidBlocksZones();
+    initFluidBlocksFlowField(snapshotStart);
+
+    version (mpi_parallel) {
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    initFullFaceDataExchange();
+    initMappedCellDataExchange();
+    initGhostCellGeometry();
+
+    writeln(format("Axisymmetric? %s", cfg.axisymmetric));
+
+    // Set everything to zero initially
+    // FIXME: This is because of some weird buffer thing in loading
+    //        data during `lmr snapshot2vtk`, it takes the same
+    //        values from the previous variable (temperature here)
+    foreach (blk; localFluidBlocks) {
+        foreach (cell; blk.cells) {
+            cell.fs.Qrad = 0.0;
+        }
+    }
+
+    string dirName = snapshotDirectory(nWrittenSnapshots);
+    ensure_directory_is_present(dirName);
+
+    FluidBlock block = localFluidBlocks[0];
+
+    uint wallSide = Face.east;
+    uint direction = opposite_face(wallSide);
+    BoundaryCondition capsuleWall = block.bc[wallSide];
+
+    trace_intensity(block, absorptionCoefficient);
+
+    foreach (blk; localFluidBlocks) {
+        auto fileName = fluidFilename(nWrittenSnapshots, blk.id);
+        FVCell[] cells;
+        cells.length = blk.cells.length;
+        foreach (i, ref c; cells) {
+            c = blk.cells[i];
+        }
+        fluidBlkIO.writeVariablesToFile(fileName, cells);
+    }
+}
--- src/radiation/source/raytrace/raytracing.d
+++ src/radiation/source/raytrace/raytracing.d
@@ -1,6 +1,7 @@
 module raytracing;
 
 // Standard modules
+import std.algorithm.iteration : sum;
 import std.conv : to;
 import std.stdio;
 import std.format;
@@ -10,6 +11,7 @@ import std.random : Random, uniform;
 
 import gas.physical_constants : StefanBoltzmann_constant;
 import geom.elements.vector3 : Vector3, wedge2D, dot;
+import geom.elements.properties : on_left_of_xy_line;
 import geom.grid.grid : Grid, Grid_t;
 import nm.number : number;
 
@@ -35,7 +37,7 @@ void trace_rays(FluidBlock block, number absorptionCoefficient) {
     auto rng = Random(4); // Chosen by fair dice roll guaranteed to be random (xkcd.com/221)
     // auto rne = mirRandom.Random(4);
     auto rne = mirRandom.Random(mirRandom.unpredictableSeed());
-    uint angleSamples = 10_000;
+    uint angleSamples = 1_000;
     auto angleGenerator = uniformVar!number(0.0, 2 * PI);
     // NOTE: Using angleSamples of 1000 causes NaN values. Weird??
 
@@ -52,16 +54,19 @@ void trace_rays(FluidBlock block, number absorptionCoefficient) {
 
     foreach (cell_id, ref origin; block.cells) {
 
-        number fullEmissionEnergy = 4 * PI * origin.volume[0] * absorptionCoefficient * origin.grey_blackbody_intensity();
-        writeln(format("[INFO ] Cell ID: %d,\t Energy: %.3g", cell_id, fullEmissionEnergy));
+        number fullEmissionEnergy = 4 * PI * origin.volume[0]
+            * absorptionCoefficient * origin.grey_blackbody_intensity();
+        // writeln(format("[INFO ] Cell ID: %d,\t Energy: %.3g", cell_id, fullEmissionEnergy));
         // origin.fs.Qrad -= fullEmission; // Remove the energy of the ray emitted
+
+        number selfAbsorbed = 0.0;
 
         foreach (a; 0 .. angleSamples) {
             // number[3] angleVector;
             // sphereVar()(rne, angleVector); // TODO: Check if this is _true_ SO3
 
             // number angle = angleGenerator(rne);
-            number angle = (double(a) / double(angleSamples) + 0.00001) * 2 * PI;
+            number angle = (double(a) / double(angleSamples) + 1E-6) * 2 * PI;
 
             // Vector3 direction = Vector3([angleVector[0], angleVector[1], 0]);
             // Vector3 direction = Vector3([angleVector[0], angleVector[1], angleVector[2]]);
@@ -78,37 +83,48 @@ void trace_rays(FluidBlock block, number absorptionCoefficient) {
             number[] rayLengths;
 
             FVInterface inter = marching_efficient(cell_id, block, direction, true, rayCells, rayLengths);
+            // FVInterface inter = marching_full(cell_id, block, direction,
+            //     true, rayCells, rayLengths);
 
-            //         writeln(format("[TRACE] Ray Cells: \t%s", rayCells));
-            //         writeln(format("[TRACE] Ray lengths: \t%s", rayLengths));
+            number trueLength = 0.0;
+            int trueCellCount = 0;
+            number thresholdEnergy = 1E-16;
+            number initialEnergy = rayEnergy;
+            number expectedLength = -log(thresholdEnergy) / absorptionCoefficient;
 
             inner: foreach (i; 0 .. rayCells.length) {
-                if (rayCells[i] == 1) {
-                    cell_1_count += 1;
-                    cell_1_length += rayLengths[i];
-                }
-                if (rayCells[i] == 4) {
-                    cell_4_count += 1;
-                    cell_4_length += rayLengths[i];
-                }
-                if (rayCells[i] == 7) {
-                    cell_7_count += 1;
-                    cell_7_length += rayLengths[i];
-                }
-
                 number cellVolume = block.cells[rayCells[i]].volume[0];
                 // Kill off the ray if it's too weak
-                if (rayEnergy < 1E-16) {
+                if (rayEnergy / initialEnergy < 1E-16) {
                     block.cells[rayCells[i]].fs.Qrad += rayEnergy / cellVolume;
+                    energyAbsorbed += rayEnergy;
+                    rayEnergy = 0.0;
                     break inner;
                 }
 
+                number expectedStep = log(rayEnergy / thresholdEnergy) / absorptionCoefficient;
+                if (expectedStep < rayLengths[i]) {
+                    trueLength += expectedStep;
+                } else {
+                    trueLength += rayLengths[i];
+                }
+
+                trueCellCount += 1;
+
                 number opticalThickness = absorptionCoefficient * rayLengths[i] / mu;
                 number heating = rayEnergy * (1 - exp(-opticalThickness));
+                // if (rayCells[i] == 1) {
+                //     writeln(format("Recieved %.3g from cell %d", heating, cell_id));   
+                // }
+                if (rayCells[i] == cell_id) {
+                    selfAbsorbed += heating;
+                }
                 energyAbsorbed += heating;
                 rayEnergy *= exp(-opticalThickness);
                 block.cells[rayCells[i]].fs.Qrad += heating / cellVolume;
             }
+
+            // writeln(format("[TRACE] Cells: %d, True length: %.3g, Expected length: %.3g", trueCellCount, trueLength, expectedLength));
 
             // writeln(format("Interface hit: %s", block.bc[inter.bc_id]));
 
@@ -121,13 +137,12 @@ void trace_rays(FluidBlock block, number absorptionCoefficient) {
 
             // progressBar.next();
         }
+
+        writeln(format("[TRACE] Cell %d, self absorbed: %.6g%%", cell_id,
+                selfAbsorbed / fullEmissionEnergy * 100));
     }
 
     progressBar.finish();
-
-    writeln(format("[DEBUG] Cell 1 count: %d, length: %.3f", cell_1_count, cell_1_length));
-    writeln(format("[DEBUG] Cell 4 count: %d, length: %.3f", cell_4_count, cell_4_length));
-    writeln(format("[DEBUG] Cell 7 count: %d, length: %.3f", cell_7_count, cell_7_length));
 
     writeln(format("Energy emitted: %.3g", energyEmitted));
     writeln(format("Energy absorbed: %.3g", energyAbsorbed));
@@ -135,10 +150,59 @@ void trace_rays(FluidBlock block, number absorptionCoefficient) {
     writeln(format("Discrepency: %.3g", energyEmitted - energyLost - energyAbsorbed));
 }
 
-FVInterface marching_efficient(
-        size_t cellID, FluidBlock block, Vector3 rayTangent, bool axisymmetric,
-        ref size_t[] crossed, ref number[] lengths
-) {
+void trace_intensity(FluidBlock block, number absorptionCoefficient) {
+    // auto rng = Random(4); // Chosen by fair dice roll guaranteed to be random (xkcd.com/221)
+    // auto rne = mirRandom.Random(4);
+    // auto rne = mirRandom.Random(mirRandom.unpredictableSeed());
+    uint angleSamples = 1_000;
+    number anglePatch = 4 * PI / double(angleSamples);
+    // auto angleGenerator = uniformVar!number(0.0, 2 * PI);
+
+    Bar progressBar = new Bar();
+    progressBar.message = { return "Ray-tracing progress"; };
+    progressBar.suffix = { return format("%6.2f%%\n", progressBar.percent); };
+    progressBar.max = angleSamples * block.cells.length;
+
+    number expectedIntensity = block.cells[0].grey_blackbody_intensity();
+    writeln(format("Expected intensity: %.3g", expectedIntensity));
+
+    foreach (cell_id, ref origin; block.cells) {
+
+        number incidentRadiation = 0.0;
+
+        foreach (a; 0 .. angleSamples) {
+            number angle = (double(a) / double(angleSamples) + 1E-6) * 2 * PI;
+            Vector3 direction = Vector3([0.0, cos(angle), sin(angle)]);
+
+            size_t[] rayCells;
+            number[] rayLengths;
+
+            FVInterface _ = marching_efficient(cell_id, block, direction, true, rayCells, rayLengths);
+            // FVInterface inter = marching_full(cell_id, block, direction, true, rayCells, rayLengths);
+
+            number opticalDistance = 0.0;
+            foreach (i; 0 .. rayCells.length) {
+                number opticalThickness = absorptionCoefficient * rayLengths[i];
+                number uniformIntensity = block.cells[rayCells[i]].grey_blackbody_intensity();
+                number exitIntensity = uniformIntensity * (1 - exp(-opticalThickness));
+                number impactedIntensity = exitIntensity * exp(-opticalDistance);
+                // G = integral vec(I) dot dd(vec(Omega))
+                // vec(q) = integral vec(I) dd(Omega)
+                incidentRadiation += impactedIntensity * anglePatch;
+                opticalDistance += opticalThickness;
+            }
+
+            progressBar.next();
+        }
+
+        origin.fs.Qrad = -absorptionCoefficient * (4*PI * origin.grey_blackbody_intensity() - incidentRadiation);
+    }
+
+    progressBar.finish();
+}
+
+FVInterface marching_efficient(size_t cellID, FluidBlock block,
+    Vector3 rayTangent, bool axisymmetric, ref size_t[] crossed, ref number[] lengths) {
     // Placeholder for if we need to do moving grids
     size_t gtl = 0;
 
@@ -159,8 +223,7 @@ FVInterface marching_efficient(
     Ray ray;
     if (axisymmetric) {
         ray = new HyperbolicRay(rayTangent, rayCoord);
-    }
-    else {
+    } else {
         ray = new CartesianRay(rayTangent, rayCoord);
     }
 
@@ -237,8 +300,7 @@ FVInterface marching_efficient(
 
             // WARN: The `currentCell` can be null if crossing a boundary
             //       with no ghost cell on the other side
-            currentCell = (currentCell.outsign[iface_id] == +1)
-                ? outgoing.right_cell : outgoing.left_cell;
+            currentCell = (currentCell.outsign[iface_id] == +1) ? outgoing.right_cell : outgoing.left_cell;
 
             // debug {
             //     writeln("Stepping ", step_length, " in direction ", rayTangent);
@@ -279,23 +341,27 @@ FVInterface marching_efficient(
  * Returns:
  *   The interface which was intersected
  */
-FVInterface marching_full(
-        size_t cellID, FluidBlock block, Vector3 rayTangent, bool axisymmetric,
-        ref size_t[] crossed, ref number[] lengths
-) {
+FVInterface marching_full(size_t cellID, FluidBlock block, Vector3 rayTangent,
+    bool axisymmetric, ref size_t[] crossed, ref number[] lengths) {
+    // Placeholder for if we need to do moving grids
+    size_t gtl = 0;
+
     bool isWithinBlock = true;
     bool isContained;
     FluidFVCell currentCell = block.cells[cellID];
     Vector3 rayCoord = currentCell.pos[0]; // INFO: Grid is stationary, index is time
     Grid currentGrid = get_grid(block);
 
-    Ray ray;
-    if (axisymmetric) {
-        ray = new HyperbolicRay(rayTangent, rayCoord);
-    }
-    else {
-        ray = new CartesianRay(rayTangent, rayCoord);
-    }
+    // Ray ray;
+    // if (axisymmetric) {
+    //     ray = new HyperbolicRay(rayTangent, rayCoord);
+    // }
+    // else {
+    //     ray = new CartesianRay(rayTangent, rayCoord);
+    // }
+
+    auto hRay = new HyperbolicRay(rayTangent, rayCoord);
+    auto cRay = new CartesianRay(rayTangent, rayCoord);
 
     number stepSize = 1E-03; // FIXME: This should be proportional to something...
     int consecutiveSteps = 1;
@@ -304,10 +370,18 @@ FVInterface marching_full(
     // NOTE: Do we want to record the first step?
 
     marchLoop: while (isWithinBlock) {
-        ray.walkForward(stepSize);
-        rayCoord += stepSize * rayTangent;
+        hRay.walkForward(stepSize);
+        cRay.walkForward(stepSize);
+        Vector3 hRayCoord = hRay.currentPoint();
+        Vector3 cRayCoord = cRay.currentPoint();
+        rayCoord = Vector3(cRayCoord.x, sqrt(cRayCoord.y ^^ 2 + cRayCoord.z ^^ 2));
+        assert((fabs(hRayCoord.x - rayCoord.x) < 1e-6) && (fabs(hRayCoord.y - rayCoord.y) < 1e-6)
+                && (fabs(hRayCoord.z - rayCoord.z) < 1e-6),
+            format("Rays are not equal. %s != %s", hRayCoord, rayCoord));
+        rayCoord = hRayCoord;
         isContained = currentGrid.point_is_inside_cell(rayCoord, currentCell.id);
 
+        // Keep walking while within the same cell
         if (isContained) {
             consecutiveSteps++;
             continue;
@@ -318,18 +392,39 @@ FVInterface marching_full(
         lengths ~= stepSize * consecutiveSteps;
         consecutiveSteps = 1;
 
+        debug {
+            if ((crossed.length >= 4) && (crossed[$ - 1] == crossed[$ - 3])
+                && (crossed[$ - 2] == crossed[$ - 4])) {
+                throw new Exception("Loop found within ray-tracing, exiting.");
+            }
+        }
+
+        Vector3 vertex_i, vertex_j;
+
         // NOTE: Could shortcut by checking the boundaries directly from the cell
         //       vertices, rather than bringing all neighbours into memory
-        foreach (i, neighbour; currentCell.cell_cloud) {
-            if (neighbour.is_ghost) {
-                // Need a different way to check if we're inside the ghost cell
-                size_t nvtx = neighbour.vtx.length;
-                writeln("Ghost cell has ", nvtx, " vertices.");
-                if (true) {
-                    hit = currentCell.iface[i - 1]; // 0th cell is self
-                    break marchLoop;
-                }
+        faces: foreach (n, iface; currentCell.iface) {
+            vertex_i = iface.vtx[0].pos[gtl];
+            vertex_j = iface.vtx[1].pos[gtl];
+            // Need to ensure the interface is moving anti-clockwise with 
+            // respect to the cell interior
+            if (currentCell.outsign[n] == -1) {
+                swap(vertex_i, vertex_j);
             }
+
+            if (on_left_of_xy_line(vertex_i, vertex_j, rayCoord)) {
+                // We haven't cross this interface (I think...)
+                continue faces;
+            }
+
+            // So we have crossed this interface ...
+            auto neighbour = currentCell.cell_cloud[n + 1];
+
+            // Check if it's a ghost
+            if (neighbour.is_ghost) {
+                hit = iface;
+                break marchLoop;
+            }  // Check that we're inside it
             else {
                 isContained = currentGrid.point_is_inside_cell(rayCoord, neighbour.id);
                 if (isContained) {
